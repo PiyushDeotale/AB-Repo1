9289a57582e86e017e5f98f986082b04c66a3be2
Piyush Deotale <piyushdeotale04@gmail.com>
2024-06-04 16:10:48 +0530

    Hello Everyone

---
+++ form.js
@@ -0,0 +1,7 @@
+const form = (fname, lname)=>{
+    return fname + lname
+};
+
+console.log(form("Piyush", "Deotale"));
+console.log(form("Sejal" + "Kashti"));
+console.log(form("Rehan" + "Deotale"));
